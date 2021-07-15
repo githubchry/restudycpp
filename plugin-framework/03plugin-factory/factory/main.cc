72883b8406b0584b7f4ef42ad7a52a72283cec27
chry <a8512413@163.com>
2021-07-15 22:23:58 +0800

    * 插件工厂增加一些思路

--- plugin-framework/03plugin-factory/factory/main.cc
+++ plugin-framework/03plugin-factory/factory/main.cc
@@ -22,10 +22,7 @@ void dump_backtrace() {
 
     // 将栈帧信息转化为字符串
     char **stack_frame_string_buffer = backtrace_symbols(stack_frame_buffer, stack_frames_size);
-    if (stack_frame_string_buffer == NULL) {
-        perror("backtrace_symbols");
-        exit(-1);
-    }
+    assert_param(stack_frame_string_buffer);
 
     // 遍历打印栈帧信息
     for (int i = 0; i < stack_frames_size; i++) {
@@ -75,13 +72,20 @@ int main() {
     signal(SIGSEGV, signal_handler);
 
 #ifdef ENABLE_LOG
+    /* TODO:
+     * 想法：是否需要把日志划分为运行日志，操作日志，错误日志？
+     * 如果分开，连贯性没了
+     * 如果不分，查找难度大
+     *
+     * 该问题代码无关，修改zlog配置文件即可实现，保留现有设计，平时写代码稍微注意一下打印类别即可
+     */
     scoped_zlog zlog_init("zlog.conf", "my_cat");
 #endif
 
 #ifdef DYNAMIC_LOAD_MODE
     log_debug("动态加载模式\n");
 #else
-    log_debug("静态加载模式\n");
+    log_debug("静态链接模式\n");
 #endif
 
     log_debug("debug\n");
@@ -91,13 +95,44 @@ int main() {
     log_error("error\n");
     log_fatal("fatal\n");
 
+    /*
+     * TODO: 队列的类型选择目前只有一种普通队列
+     * 普通队列，满则堵塞
+     * 环形队列，仅缓存最新N个数据，新的替换旧的
+     * 优先级队列，紧急数据可能会插入到队列前面
+     * 无锁队列，极端情况下，性能跟不上，可能需要使用CAS实现
+     */
     Queue<ProtocolDataVar *> queue(100);
 
+    // TODO: 除了队列，各插件通用的还有内存池，线程池，配置文件等
+
+    /*
+     * 主进程跑流水线任务，配置谁负责管理？BS结构还需要设计一个 webserver
+     * main和webserver之间就需要进程间的通讯 => 配置更新实时生效
+     *
+     * 但是各种配置的读写肯定是在各插件的代码实现，难道webserver进程也要加载一次各插件？是否多此一举了？
+     * 主进程同时负责webserver和woker？这样就省下了不少资源，也不用去搞进程间通信
+     * 复杂度更高了吗？ 不复杂！进程间通信才复杂
+     */
+
+
+    /*
+     * TODO: 插件类型一般不只这两种，可以从很多种层面划分
+     * 串口 <==透传==> 网络
+     *      串口读 ==上行==>  网络发
+     *      网络收 ==下行==>  串口写
+     *
+     * 采集 => 计算 => 加工 => 存储 => 平台对接(上传)
+     *                    => 报警 => 平台对接(上传)
+     *
+     * 需要根据需求自行设计插件类型PluginModule和流水线Worker
+     */
     Collector::Instance().SetDataQueue(&queue);
-    Collector::Instance().Init();   // 对该类别下各插件逐个设置 logger queue
+    Collector::Instance().Init();
 
     Processor::Instance().Init();
 
+    // TODO: worker不应该仅一个，起码两个保证数据上行和下行
     DataWorker worker(&queue);
     worker.Start();
 
